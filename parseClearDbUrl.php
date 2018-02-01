e78dbceb11b30eddc5535336b8c716fb89efe194
Clement Barbaza <clement.barbaza@gmail.com>
2018-02-01 19:03:40 +0100

    Check arg

--- parseClearDbUrl.php
+++ parseClearDbUrl.php
@@ -1,6 +1,10 @@
 <?php
 
-$url = parse_url($argv[1]);
-print_r($url);
+if (!empty($argv[1])) {
+    $url = parse_url($argv[1]);
+    print_r($url);
+} else {
+    echo "\nPlease enter your CLEARDB_DATABASE_URL environment variable.\nE.g. php -f parseClearDbUrl.php 'CLEARDB_DATABASE_URL' \n\n";
+}
 
 ?>
